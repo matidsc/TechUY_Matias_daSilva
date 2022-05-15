89c463bc362a02f3fc520ca65a1bf43ae8e61828
matidsc <dmatiutu211102@gmail.com>
2022-05-15 02:42:28 -0300

    Arreglo de bugs visuales

--- src/components/resetScroll.jsx
+++ src/components/resetScroll.jsx
@@ -3,7 +3,7 @@ import { useLocation } from "react-router-dom";
 
 const ResetScroll=()=> {
   const { pathname } = useLocation();
-
+    
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
